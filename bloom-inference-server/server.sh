4e6624af63f1bda30f99998b400cf9a7dd1c9b09
mayank31398 <mayank31398@gmail.com>
2022-09-18 22:12:27 +0530

    flask app

--- bloom-inference-server/server.sh
+++ bloom-inference-server/server.sh
@@ -2,4 +2,4 @@ export MODEL_NAME=bigscience/bloom
 export DEPLOYMENT_FRAMEWORK=hf_accelerate
 export DTYPE=fp16
 
-gunicorn -w 1 -b 127.0.0.1:5000 server:app
+gunicorn -t 0 -w 1 -b 127.0.0.1:5000 server:app
