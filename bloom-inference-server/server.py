ce08213ecb52e601c26739255d06f861fade0329
mayank31398 <mayank31398@gmail.com>
2022-09-18 22:56:42 +0530

    fix args

--- bloom-inference-server/server.py
+++ bloom-inference-server/server.py
@@ -19,13 +19,14 @@ class QueryID(BaseModel):
 
 # placeholder class for getting args. gunicorn does not allow passing args to a
 # python script via ArgumentParser
-class Args(BaseModel):
-    deployment_framework: str = os.getenv(
-        "DEPLOYMENT_FRAMEWORK", HF_ACCELERATE)
-    model_name: str = os.getenv("MODEL_NAME")
-    dtype: torch.dtype = get_torch_dtype(os.getenv("DTYPE"))
-    allowed_max_new_tokens: int = os.getenv("ALLOWED_MAX_NEW_TOKENS", 100)
-    debug: bool = parse_bool(os.getenv("DEBUG", "false"))
+class Args:
+    def __init__(self) -> None:
+        self.deployment_framework = os.getenv(
+            "DEPLOYMENT_FRAMEWORK", HF_ACCELERATE)
+        self.model_name = os.getenv("MODEL_NAME")
+        self.dtype = get_torch_dtype(os.getenv("DTYPE"))
+        self.allowed_max_new_tokens = os.getenv("ALLOWED_MAX_NEW_TOKENS", 100)
+        self.debug = parse_bool(os.getenv("DEBUG", "false"))
 
 
 # ------------------------------------------------------
