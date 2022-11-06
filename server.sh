7c3099b5b75531dd9a5e7d2d8d5ac8c1f8bfd29a
mayank31398 <mayank31398@gmail.com>
2022-11-06 06:47:21 +0530

    test

--- server.sh
+++ server.sh
@@ -3,6 +3,7 @@ export MODEL_NAME=bigscience/bloom
 export DEPLOYMENT_FRAMEWORK=hf_accelerate
 export DTYPE=fp16
 export MAX_INPUT_LENGTH=2048
+export NUM_GPUS=8
 
 # for more information on gunicorn see https://docs.gunicorn.org/en/stable/settings.html
 gunicorn -t 0 -w 1 -b 127.0.0.1:5000 inference_server.server:app --access-logfile - --access-logformat '%(h)s %(t)s "%(r)s" %(s)s %(b)s'
