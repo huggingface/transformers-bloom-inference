4408e81cdd351f88a62420991cfaeb441e3e9cad
mayank31398 <mayank31398@gmail.com>
2022-11-06 09:32:30 +0530

    test

--- inference_server/models/model.py
+++ inference_server/models/model.py
@@ -85,9 +85,9 @@ def get_downloaded_model_path(model_name: str):
         repo_id=model_name,
         local_files_only=is_offline_mode(),
         cache_dir=os.getenv("TRANSFORMERS_CACHE", None),
-        allow_patterns=["*.pt", "*.bin", "*.json"],
+        allow_patterns=["*.pt", "*.bin"],
         # maybe move to safetensors in the future
-        ignore_patterns=["*.safetensors"],
+        ignore_patterns=["*.safetensors", "*log*", "*evaluation*", "tensorboard"],
     )
     # download only on 1 process
     run_rank_n(f, barrier=True)
